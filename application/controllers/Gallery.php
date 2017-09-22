c17d6bf2b46fee47e0587acaf009883fdd303b3b
Darren Chan <chanthedarren@gmail.com>
2017-09-22 12:21:04 -0700

    added new features
    Signed-off-by: Darren Chan <chanthedarren@gmail.com>

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Gallery extends Application
 {
-
 	/**
 	 * Index Page for this controller.
 	 *
@@ -17,12 +16,29 @@ class Gallery extends Application
 	 * map to /welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	 
 	public function index()
 	{
 		
-		//$this ->load ->view("gallery");
-		$this->data['pagebody'] = 'gallery';
-		$this->render(); 
+	$pix = $this->images->all();
+	 
+	foreach ($pix as $picture)
+		$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+		
+	//prime table class
+	$this->load->library('table');
+	$parms = array(
+		'table_open' => '<table class ="gallery">',
+		'cell_start' => '<td class = "oneimage">',
+		'cell_alt_start' => '<td class = "oneimage">'
+		);
+	$this->table->set_template($parms);
+	
+	//generate table
+	$rows = $this->table->make_columns($cells, 3);
+	$this->data['thetable'] = $this->table->generate($rows);
+	
+	$this->data['pagebody'] = 'gallery';
+	$this->render();
 	}
-
 }
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,10 +19,26 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+	$pix = $this->images->newest();
+	 
+	foreach ($pix as $picture)
+		$cells[] = $this->parser->parse('_cell', (array) $picture, true);
 		
-		//$this ->load ->view("welcome");
-		$this->data['pagebody'] = 'welcome';
-		$this->render(); 
+	//prime table class
+	$this->load->library('table');
+	$parms = array(
+		'table_open' => '<table class ="gallery">',
+		'cell_start' => '<td class = "oneimage">',
+		'cell_alt_start' => '<td class = "oneimage">'
+		);
+	$this->table->set_template($parms);
+	
+	//generate table
+	$rows = $this->table->make_columns($cells, 3);
+	$this->data['thetable'] = $this->table->generate($rows);
+	
+	$this->data['pagebody'] = 'gallery';
+	$this->render();
 	}
 
 }
